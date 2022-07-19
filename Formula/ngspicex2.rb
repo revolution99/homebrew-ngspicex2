db6140378b2460b35f972fe6ecc02a2d68d45453
joe <joe@revolutioncustomshop.com>
2022-07-19 10:04:18 -0400

    ngspice with x v5 (modified formula for source build)

--- Formula/ngspicex2.rb
+++ Formula/ngspicex2.rb
@@ -9,14 +9,6 @@ class Ngspicex2 < Formula
     regex(%r{url=.*?/ngspice[._-]v?(\d+(?:\.\d+)*)\.t}i)
   end
 
-  bottle do
-    sha256 arm64_monterey: "6421f66dabcafb24914972884fc7e4ff4dfd2919b0898864ad2580e9528d5c4b"
-    sha256 arm64_big_sur:  "9222829c0bfa71e306e073be15f9c4976dfa105480b618c842823978d3cf4e23"
-    sha256 monterey:       "80a8fc2742bca92a86cb22062ad7dfb1180769ec185be23807e48ae009f92143"
-    sha256 big_sur:        "9e0a218271ee9b1491d4e896c77905589d44464c13a99db0b946a38068583e6e"
-    sha256 catalina:       "f67eb8cb4eddb299a848a22e2507cf3fdb775b17f15dd1c5451186fc1387ab4c"
-    sha256 x86_64_linux:   "27e1a76a94f39071ef949f848ed17d7a59b3a0db7c2d444f846b5f1e85e0ce2c"
-  end
 
   head do
     url "https://git.code.sf.net/p/ngspice/ngspice.git", branch: "master"
@@ -27,11 +19,15 @@ class Ngspicex2 < Formula
     depends_on "libtool" => :build
   end
 
+  depends_on "autoconf" => :build
+  depends_on "automake" => :build
+  depends_on "bison" => :build
+  depends_on "libtool" => :build
   depends_on "fftw"
   depends_on "readline"
 
   def install
-    system "./autogen.sh" if build.head?
+    system "./autogen.sh"
 
     args = %W[
       --disable-dependency-tracking
